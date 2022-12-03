0c3b5cfba7ddf2fb6bed3efd77a7fb553dd0cf04
Omar Amr <63269388+Omar125X@users.noreply.github.com>
2022-12-03 10:57:22 +0300

    feat: try component

--- components/test/test.tsx
+++ components/test/test.tsx
@@ -1,4 +1,55 @@
-import { Button } from '@nextui-org/react';
+import { Grid, Card, Text } from "@nextui-org/react";
 
-const Component = () => <Button>Click me</Button>;
-export default Component;
+export default function Component() {
+  const MockItem = ({ text }) => {
+    return (
+      <Card css={{ h: "$20", $$cardColor: '$colors$primary' }}>
+        <Card.Body>
+          <Text h6 size={15} color="white" css={{ m: 0 }}>
+            {text}
+          </Text>
+        </Card.Body>
+      </Card>
+    );
+  };
+  return (
+    <Grid.Container gap={8.2} justify="center">
+      <Grid xs={6}>
+        <MockItem text="1 of 2" />
+      </Grid>
+      <Grid xs={6}>
+        <MockItem text="2 of 2" />
+      </Grid>
+      <Grid xs={6}>
+        <MockItem text="1 of 3" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="2 of 3" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="3 of 3" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="1 of 4" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="2 of 4" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="3 of 4" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="4 of 4" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="1 of 3" />
+      </Grid>
+      <Grid xs={6}>
+        <MockItem text="2 of 3" />
+      </Grid>
+      <Grid xs={3}>
+        <MockItem text="3 of 3" />
+      </Grid>
+    </Grid.Container>
+  );
+}
